78fd923cfcf53308d6e81065c8ce3bd204a1cbf9
pgsfernandes <pedrogsilvafernandes@gmail.com>
2024-04-02 17:45:41 +0100

    added missing function to export

--- src/SpinningBlackHoles.jl
+++ src/SpinningBlackHoles.jl
@@ -26,6 +26,6 @@ include("ISCO.jl")
 
 include("CurvatureScalars.jl")
 
-export Field, LoadSystem, PrintData, interpolate1D, interpolate1D!, interpolate, interpolate!, GetMass, GetJ, GetTh, GetAh, GetωχKerr, GetωχKerrN, quantities_kerr, GetLe, GetLp, Sphericity, vH, get_quantities, fKerrN, gKerrN, hKerrN, WKerrN, AtKerrN, AφKerrN, Print_Petrov, gtt, gtphi, gphiphi, grr, gthetatheta, Ergosphere, CircumferencialRadius, LightRing, ISCO, Ricci_Horizon, GB_Horizon, RicciScalar, GBScalar, X, Mx, Y, My, Nx, Ny
+export Field, LoadSystem, PrintData, write_data_to_file, interpolate1D, interpolate1D!, interpolate, interpolate!, GetMass, GetJ, GetTh, GetAh, GetωχKerr, GetωχKerrN, quantities_kerr, GetLe, GetLp, Sphericity, vH, get_quantities, fKerrN, gKerrN, hKerrN, WKerrN, AtKerrN, AφKerrN, Print_Petrov, gtt, gtphi, gphiphi, grr, gthetatheta, Ergosphere, CircumferencialRadius, LightRing, ISCO, Ricci_Horizon, GB_Horizon, RicciScalar, GBScalar, X, Mx, Y, My, Nx, Ny
 
 end
